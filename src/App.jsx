9faedd7fa95c5e5bb900b1709779b1e8d6c4602e
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-02-16 00:37:31 +0100

    removed images

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,13 @@ import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import logo from './logo.svg'
 import './App.css'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
-import MainContent from './Components/MainContent'
-import Info from './Components/Info/Info'
-import Button from './Components/Info/Button'
-import About from './Components/About'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Header/>
-      <Info />
-      <Button />
-      <About />
-      <Footer/>
+      welcome
     </div>
   )
 }
